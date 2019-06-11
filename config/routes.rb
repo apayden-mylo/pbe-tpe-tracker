997e368e93319f7c6801d6a5b6f798ef079fdb41
AP047572 <addison.payden@cerner.com>
2019-06-11 15:12:48 -0500

    Fixed a blunder

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   resources :players, only: [:index, :show], path_names: { index: 'overview' }
   resources :teams, only: :show
 
-  get 'teams'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
